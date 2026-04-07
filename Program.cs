8c9f0a90aee37cf5ebe6764372605c9214b4b5d9
stteenArts <stteenriwi@gmail.com>
2026-04-06 21:31:52 -0500

    create: the level until the 5 level are created and reestructure the logic class

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@
 // The control of ours system
 bool cycle = true;
 Arithmetic viewResult = new Arithmetic();
-
 while (cycle)
 {
     Console.WriteLine("");
@@ -17,9 +16,8 @@ while (cycle)
         Nivel 5: Análisis Numérico (Listas de enteros)
         Nivel 6: Gestión de Tareas (Listas de cadenas)
         Nivel 7: Arreglos y Búsqueda
-        Nivel 8: Programación Orientada a Objetos (Clases)
-        Nivel 9: CRUD de Objetos (Listas de objetos)
-        Nivel 10: Salir
+        Nivel 8: Colecciones de Objetos (Clases List<estudiante>)
+        Nivel 9: Salir
     ");
     Console.Write("Seleccione un nivel para iniciar >: ");
     string choise = Console.ReadLine()!;
@@ -46,19 +44,61 @@ while (cycle)
             Console.Write("Porfavor digite el segundo numero: ");
             bool validate2 =float.TryParse(Console.ReadLine(), out float num2);
             
-            float result = Arithmetic.levelBasic(num1, num2);
-            viewResult.showResult(result, validate1, validate2);
+            float resultPlus = Arithmetic.plusLevel(num1, num2);
+            viewResult.showResult(resultPlus, validate1, validate2);
             
             break;
         case 2:
-          
+            Console.WriteLine("Nivel 2: Calculo y condicionales");
+            Console.WriteLine("");
+            
+            // Form request numbers
+            Console.Write("Porfavor digite la (Primera) calificación: ");
+            bool response1 = float.TryParse(Console.ReadLine(), out float average1);
+            Console.Write("Porfavor digite la (Segunda) calificación: ");
+            bool response2 = float.TryParse(Console.ReadLine(), out float average2);
+            Console.Write("Porfavor digite la (Tercera) calificación: ");
+            bool response3 = float.TryParse(Console.ReadLine(), out float average3);
+            
+            // Operational Mathematics
+            double averageResult = Arithmetic.averageLevel(average1, average2, average3);
+            viewResult.showResult(averageResult, response1, response2,response3);
+            
             break;
         case 3:
-           
+            Console.WriteLine("Nivel 3: Manipulación de Cadenas");
+            Console.WriteLine("");
+            
+            // Form request names
+            Console.Write("Porfavor digite su primer Nombre: ");
+            string firstName = Console.ReadLine()!;
+            Console.Write("Porfavor digite su segundo Nombre: ");
+            string secondName = Console.ReadLine()!;
+            
+            // Meeting
+            viewResult.meetingLevel(firstName, secondName);
             
             break;
         case 4:
+            Console.WriteLine("Nivel 4: Operaciones Lógicas y Control de Flujo");
+            Console.WriteLine("");
             
+            // Form request numbers
+            Console.Write("Porfavor digite el primer número: ");
+            bool calc1 = float.TryParse(Console.ReadLine(), out float number1);
+            Console.Write("Porfavor digite el segundo número: ");
+            bool calc2 = float.TryParse(Console.ReadLine(), out float number2);
+            Console.Write("Porfavor digite el tipo de calculo (+, -, /, *): ");
+            string type = Console.ReadLine()!;
+            viewResult.calculatorLevel(number1, number2, type, calc1, calc2);
+            
+            break;
+        case 5:
+            Console.WriteLine("Nivel 5: Análisis Numérico");
+            Console.WriteLine("");
+            
+            // Form request numbers
+            LogicList.ListNumbers();
             break;
         default:
             Console.WriteLine("Porfavor seleccione una opción valida ");
--- modules/arithmetic.cs
+++ modules/arithmetic.cs
@@ -2,13 +2,15 @@ namespace Modules.arithmetic;
 
 public class Arithmetic
 {
-    public float firstNum;
-    public float secondNum;
+    public float FirstNum;
+    public float SecondNum;
 
-
-    public void showResult(float result, bool num1, bool num2)
+    //Function type void to show resulting's operation
+    // This validate for that the result will be of type correct (float and numbers)
+    public void showResult(dynamic result, params bool[] numbers)
     {
-        if (num1 == true && num2 == true)
+        // condition to validate if they are numbers
+        if (numbers.All(x => x == true))
         {
             // Show the result in console 
             Console.Clear();
@@ -16,16 +18,99 @@ public class Arithmetic
             Console.WriteLine($"El resultado es: {result}");
             Console.WriteLine("");
         }
+        // In case that doesn't return in console a message
         else
         {
+            Console.WriteLine("");
             Console.WriteLine($"Error! No se puede sumar letras y numeros");
         }
 
     }
-    public static float levelBasic(float firstNum, float secondNum)
+    // Function static:  operate plus in the system
+    //Level 1: "Manipulación Numérica Básica"
+    public static float plusLevel(float firstNum, float secondNum)
     {
         float plus = firstNum + secondNum;
         // Console.WriteLine(plus);
         return plus;
+    }
+    
+    // Function static: Operate average in the system
+    public static double averageLevel(float average1, float average2, float average3)
+    {
+        float averages = average1 + average2 + average3;
+        double average = averages / 3;
+        return average;
+    }
+
+    public void meetingLevel(string firstName, string secondName)
+    {
+        Console.WriteLine($"Hola! {firstName}, {secondName}, es un gusto saludarte");
+    }
+
+    public void calculatorLevel(float firstNum, float secondNum, string typeOp, params bool[] numbers)
+    {
+        switch (typeOp)
+        {
+            case "+":
+
+                float resultPlus = plusLevel(firstNum, secondNum);
+                showResult(resultPlus, numbers);
+                break;
+            case "-":
+                float resultRest = firstNum - secondNum;
+                showResult(resultRest, numbers);
+                break;
+            case "*":
+                float resultMultiplication = firstNum * secondNum;
+                showResult(resultMultiplication, numbers);
+                break;
+            case "/":
+                float resultDivide = firstNum / secondNum;
+                showResult(resultDivide, numbers);
+                break;
+            default:
+                Console.WriteLine("No se encontro signo logico para operar");
+                break;
+        }
+    }
+
+    public static List<int> listLevel(params int[] numbers)
+    {
+        List<int> listNumbers = new List<int>();
+        foreach (var number in numbers)
+        {
+            listNumbers.Add(number);
+        }
+        Console.WriteLine($"Se han añadido los numeros a la lista ");
+        return listNumbers;
+    }
+}
+
+public static class LogicList
+{
+    public static void ListNumbers()
+    {
+        int sum = 0;
+        Console.Write("Porfavor digite un numero: ");
+        bool response1 = int.TryParse(Console.ReadLine(), out int number1);
+        Console.Write("Porfavor digite un numero: ");
+        bool response2 = int.TryParse(Console.ReadLine(), out int number2);
+        Console.Write("Porfavor digite un numero: ");
+        bool response3 = int.TryParse(Console.ReadLine(), out int number3);
+        Console.Write("Porfavor digite un numero: ");
+        bool response4 = int.TryParse(Console.ReadLine(), out int number4);
+        Console.Write("Porfavor digite un numero: ");
+        bool response5 = int.TryParse(Console.ReadLine(), out int number5);
+        List<int> listNumber = Arithmetic.listLevel(number1, number2, number3, number4, number5);
+        
+        foreach (var number in listNumber)
+        {
+            sum += number;    
+        }
+        Console.WriteLine("");
+        Console.WriteLine($"La suma de la lista de numeros es: {sum}");
+        Console.WriteLine($"El numero mayor: {listNumber.Max()}");
+        Console.WriteLine($"El numero menor: {listNumber.Min()}");
     }
 }
